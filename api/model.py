21ce9b56cc6226ef5103495a2412e99b8551e452
Zachary Leonard <150874900+zaleonard@users.noreply.github.com>
2025-02-18 21:03:20 -0800

    Delete api directory

--- api/model.py
+++
@@ -1,7 +0,0 @@
-#yolo8 - look at yolo schema for proper structure
-#find models on the website
-from ultralytics import YOLO
-import os
-
-model = YOLO('yolov8n-cls.pt')
-model.train(data=r'fashion-mnist', epochs = 30, imgsz =28)
