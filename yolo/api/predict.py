b1f5174d7fa7907920feb7f1dc1e89e2edc3b750
Zachary Leonard <150874900+zaleonard@users.noreply.github.com>
2025-02-18 21:07:43 -0800

    Update predict.py

--- yolo/api/predict.py
+++ yolo/api/predict.py
@@ -4,10 +4,8 @@ import cv2
 model = YOLO('./runs/classify/train10/weights/last.pt')
 image_path = r'./test_image/OIP.jpg'
 input_image = cv2.imread(image_path)
-# input_image = cv2.resize(input_image, (28,28))
+input_image = cv2.resize(input_image, (28,28))
 results = model.predict(input_image)
 
 for result in results:
-    #shows the confidence scores for class IDs
-
     result.show()
